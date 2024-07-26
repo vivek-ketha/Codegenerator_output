cac73324c8aab20295044e945678c54befa4425b
Vivek Ketha <90147332+vivek-ketha@users.noreply.github.com>
2024-07-26 15:27:09 +0530

    Delete output directory

--- output/.mvn/wrapper/MavenWrapperDownloader.py
+++
@@ -1,9 +0,0 @@
-
-from playwright.sync_api import Page, sync_playwright
-
-def download_file_from_url(url: str, destination: str):
-    with sync_playwright() as playwright:
-        with playwright.chromium.launch() as browser:
-            page = browser.new_page()
-            page.goto(url)
-            page.download(destination)
