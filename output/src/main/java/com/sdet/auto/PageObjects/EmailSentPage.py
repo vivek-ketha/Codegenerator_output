7e9ba3a4ed1e276855aa3955a94b858b6fe717c5
Vivek Ketha <90147332+vivek-ketha@users.noreply.github.com>
2024-08-06 15:55:06 +0530

    Delete output directory

--- output/src/main/java/com/sdet/auto/PageObjects/EmailSentPage.py
+++
@@ -1,9 +0,0 @@
-
-from playwright.sync_api import Page
-
-class EmailSentPage:
-    txt_message = "#content"
-
-    @staticmethod
-    def verify_email_sent(page: Page, test_assert, expected_msg):
-        test_assert.set_pass(page.locator(EmailSentPage.txt_message).text_content() == expected_msg)
--- output/src/main/java/com/sdet/auto/PageObjects/Navigation.py
+++
@@ -1,5 +0,0 @@
-
-from playwright.sync_api import Page
-
-def nav_to_web_page_under_test(page: Page):
-    page.goto(ConfigSettings.get_web_url())
